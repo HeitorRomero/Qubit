f662da8aa115ea62b696a587090f8b8832dc2904
HeitorRomero <hpardinhoromero@gmail.com>
2026-09-21 22:14:19 -0400

    Simulacao de um circuito quantico simples usando Qiskit (IBM) ao inves de contruir o qubit.

--- qubit.py
+++ qubit.py
@@ -1,4 +1,19 @@
-import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler as Sampler
+from qiskit.visualization import plot_histogram
+
+qc = QuantumCircuit(2, 2) #Cria um circuito com 2 qubits e 2 estados
+qc.h(0) #coloca o qubit 0 em superposicao
+qc.cx(0, 1) #entrelaca o qubit 0 e qubit 1
+qc.measure([0, 1], [0, 1]) #faz a medicao dos qubits e guarda o resultado nos bits classicos
+print(qc.draw()) #printa o circuito
+
+
+
+
+
+
+"""import numpy as np
 
 class Qubit:
     def __init__(self, alpha=1.0, beta=0.0): #prob de estados do qubit (alfa e beta)
@@ -23,4 +38,4 @@ print("q1 está normalizado?", q1.is_normalized())
 q2 = Qubit(1.0, 1.0)
 print("q2 está normalizado?", q2.is_normalized())
 q2.normalize()
-print("q2 após normalização:", q2.state)
+print("q2 após normalização:", q2.state)"""
